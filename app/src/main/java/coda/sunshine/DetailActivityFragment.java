bf4ec0c2683fc243747ddbb212c996fbe688a801
Drellimal2 <dmiller2310@hotmail.com>
2015-06-30 09:40:09 -0500

    Share Action working, added contract

--- app/src/main/java/coda/sunshine/DetailActivityFragment.java
+++ app/src/main/java/coda/sunshine/DetailActivityFragment.java
@@ -1,7 +1,9 @@
 package coda.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,25 +13,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import static android.support.v4.view.MenuItemCompat.getActionProvider;
-
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
 
+
+    private String mforecast = "";
     public DetailActivityFragment() {
         setHasOptionsMenu(true);
     }
 
+
+    private Intent createShareIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra("FORECAST", mforecast);
+        return shareIntent;
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.details_fragment, menu);
 
         MenuItem menuItem = menu.findItem(R.id.action_share);
-        ShareActionProvider shareActionProvider = (ShareActionProvider) getActionProvider(menuItem);
+        ShareActionProvider shareActionProvider = new ShareActionProvider(getActivity());
+        MenuItemCompat.setActionProvider(menuItem, shareActionProvider);
+        shareActionProvider.setShareIntent(createShareIntent());
         super.onCreateOptionsMenu(menu, inflater);
     }
 
@@ -39,7 +51,12 @@ public class DetailActivityFragment extends Fragment {
 
         View rootView =  inflater.inflate(R.layout.fragment_detail, container, false);
         TextView forecast = (TextView) rootView.findViewById(R.id.fragment_detail_text);
-        forecast.setText(getActivity().getIntent().getStringExtra("FORECAST"));
+        Intent intent = getActivity().getIntent();
+        if( intent != null && intent.hasExtra("FORECAST")) {
+            mforecast = intent.getStringExtra("FORECAST");
+            forecast.setText(mforecast);
+        }
+
         return rootView;
     }
 }

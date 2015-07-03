fd0b5c17343c77511e6c85c0f5fa475ec5b4521b
Drellimal2 <dmiller2310@hotmail.com>
2015-07-03 08:57:08 -0500

    Added Weather Provider class and implemented Uri methods in the contract

--- app/src/main/java/coda/sunshine/data/WeatherContract.java
+++ app/src/main/java/coda/sunshine/data/WeatherContract.java
@@ -15,6 +15,8 @@
  */
 package coda.sunshine.data;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 import android.text.format.Time;
 
@@ -23,15 +25,12 @@ import android.text.format.Time;
  */
 public class WeatherContract {
 
-    // To make it easy to query for the exact date, we normalize all dates that go into
-    // the database to the start of the the Julian day at UTC.
-    public static long normalizeDate(long startDate) {
-        // normalize the start date to the beginning of the (UTC) day
-        Time time = new Time();
-        time.set(startDate);
-        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
-        return time.setJulianDay(julianDay);
-    }
+    //Content Authority
+    public static final String CONTENT_AUTHORITY  = "coda.sunshine.app";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_WEATHER = "weather";
+    public static final String PATH_LOCATION = "location";
+
 
     /*
         Inner class that defines the table contents of the location table
@@ -39,9 +38,15 @@ public class WeatherContract {
         done for WeatherEntry)
      */
     public static final class LocationEntry implements BaseColumns {
-        public static final String TABLE_NAME = "location";
 
-        public static final String COLUMN_LOC_ID = "location_id";
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
+
+        public static final String TABLE_NAME = "location";
 
         public static final String COLUMN_LOCATION_SETTING = "location_setting";
 
@@ -51,10 +56,21 @@ public class WeatherContract {
 
         public static final String COLUMN_CITY_NAME = "city_name";
 
+        public static Uri buildLocationUri(long _id){
+            return ContentUris.withAppendedId(CONTENT_URI, _id);
+        }
+
     }
 
     /* Inner class that defines the table contents of the weather table */
     public static final class WeatherEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+
 
         public static final String TABLE_NAME = "weather";
 
@@ -84,5 +100,40 @@ public class WeatherContract {
 
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
+
+        public static Uri buildWeatherUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSetting){
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(String locationSetting, String startDate){
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).appendQueryParameter(COLUMN_DATE, startDate).build();
+        }
+
+        public static String getLocationSettingFromUri(Uri uri){
+            return uri.getPathSegments().get(1);
+        }
+
+        public static String getDateFromUri(Uri uri){
+            return uri.getPathSegments().get(2);
+        }
+
+        public static String getStartDateFromUri(Uri uri){
+            return uri.getQueryParameter(COLUMN_DATE);
+        }
+
+    }
+
+    // To make it easy to query for the exact date, we normalize all dates that go into
+    // the database to the start of the the Julian day at UTC.
+    public static long normalizeDate(long startDate) {
+        // normalize the start date to the beginning of the (UTC) day
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
     }
 }

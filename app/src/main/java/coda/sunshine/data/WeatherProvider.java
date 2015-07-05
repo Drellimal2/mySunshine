c101a7c7b5ab157218935b641ada4a3c316370ce
Drellimal2 <dmiller2310@hotmail.com>
2015-07-05 15:33:33 -0500

    Finished Provider

--- app/src/main/java/coda/sunshine/data/WeatherProvider.java
+++ app/src/main/java/coda/sunshine/data/WeatherProvider.java
@@ -5,6 +5,7 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
@@ -204,25 +205,118 @@ public class WeatherProvider extends ContentProvider {
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+        final SQLiteDatabase db = weatherDbHelper.getWritableDatabase();
         switch (match) {
-//            case WEATHER: {
-//                long _id
-//                return WeatherContract.WeatherEntry.CONTENT_TYPE;
-//            }
-//            case LOCATION: {
-//                return WeatherContract.LocationEntry.CONTENT_TYPE;
-//            }
+            case WEATHER: {
+                long _id = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, values);
+                if( _id > 0 ){
+                    returnUri = WeatherContract.WeatherEntry.buildWeatherUri(_id);
+                } else{
+                    throw new android.database.SQLException("Failed to insert");
+                }
+                break;
+            }
+            case LOCATION: {
+                long _id = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, values);
+                if( _id > 0 ){
+                    returnUri = WeatherContract.LocationEntry.buildLocationUri(_id);
+                } else{
+                    throw new android.database.SQLException("Failed to insert");
+                }
+                break;
+            }
+            default:{
+                throw new android.database.SQLException("Failed to insert");
+
+            }
         }
-        return null;
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        final int match = sUriMatcher.match(uri);
+        final SQLiteDatabase db = weatherDbHelper.getWritableDatabase();
+        int rowsDeleted;
+        switch (match) {
+            case WEATHER: {
+                rowsDeleted = db.delete(WeatherContract.WeatherEntry.TABLE_NAME, selection,selectionArgs);
+                break;
+            }
+            case LOCATION: {
+                rowsDeleted = db.delete(WeatherContract.LocationEntry.TABLE_NAME, selection,selectionArgs);
+                break;
+            }
+            default:{
+                throw new android.database.SQLException("Failed to insert");
+
+            }
+        }
+        if( null == selection || 0 != rowsDeleted){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsDeleted;
+
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final int match = sUriMatcher.match(uri);
+        final SQLiteDatabase db = weatherDbHelper.getWritableDatabase();
+        switch (match) {
+            case WEATHER: {
+                db.beginTransaction();
+                int returnCount = 0;
+                try{
+                    for( ContentValues value: values){
+                        long _id = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, value);
+                        if( _id !=-1){
+                            returnCount ++;
+                        }
+
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            }
+
+            default:{
+                return super.bulkInsert(uri, values);
+
+            }
+        }
+
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        final int match = sUriMatcher.match(uri);
+        final SQLiteDatabase db = weatherDbHelper.getWritableDatabase();
+        int rowsUpdated;
+        switch (match) {
+            case WEATHER: {
+                rowsUpdated = db.update(WeatherContract.WeatherEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            }
+            case LOCATION: {
+                rowsUpdated = db.update(WeatherContract.LocationEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            }
+            default:{
+                throw new android.database.SQLException("Failed to insert");
+
+            }
+        }
+        if( null == selection || 0 != rowsUpdated){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsUpdated;
     }
 }
